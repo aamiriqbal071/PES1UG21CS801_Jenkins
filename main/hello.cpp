89fea9d3cf6b49077de9e522b9200119d0191a1d
aamiriqbal071 <39021017+aamiriqbal071@users.noreply.github.com>
2023-02-17 22:07:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Hello, I am Aamir my SRN is PES1UG21CS801\n";
   return 0;
 }
